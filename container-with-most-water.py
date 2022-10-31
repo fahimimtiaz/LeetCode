2906e8de87188b0df7bd4e55bdaeb278b2c592e7
fahim-bs23 <imtiaz.ahmed@brainstation-23.com>
2022-10-31 14:01:25 +0600

    end of array type problem

---
+++ container-with-most-water.py
@@ -0,0 +1,22 @@
+    from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height)-1
+        ans = 0
+        while(l<r):
+            area = min(height[l],height[r]) * (r-l)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+            ans = max(ans,area)
+        return ans
+            
+
+
+ans = Solution().maxArea(height = [1,1])
+print(ans)
+
+        
