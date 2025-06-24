9a242dd9d991f4c61fa08f6d25a477ad8d34fc41
imtiaz.ahmed <imtiaz.ahmed@upaybd.com>
2025-06-24 12:13:02 +0600

    optimize zigzag problem

--- zigzag-conversion.py
+++ zigzag-conversion.py
@@ -1,45 +1,74 @@
 
+# class Solution:
+#     def convert(self, s: str, numRows: int) -> str:
+#         row_size = int(len(s)/2) + 1
+#         matrix = [[0 for _ in range(row_size)] for _ in range(numRows)]
+#         row = 0
+#         column = 0
+
+#         if len(s) <= 2 or numRows==1:
+#             return s
+
+#         forward = True
+#         for i, char in enumerate(s):
+#             # print("Row: ", row, "Column: ", column)
+#             if forward:
+#                 if numRows - row != 1:
+#                     matrix[row][column] = char
+#                     row += 1
+#                 else:
+#                     matrix[row][column] = char
+#                     forward = False
+
+#             if not forward:
+#                 if row != 0:
+#                     matrix[row][column] = char
+#                     row -= 1
+#                     column += 1
+#                 else:
+#                     matrix[row][column] = char
+#                     row += 1
+#                     forward = True
+            
+#         new_str = ""
+#         for row in matrix:
+#             for value in row:
+#                 if value != 0:
+#                     new_str += value
+#                     # print(value, end = " ")
+#             # print()
+
+#         return new_str
+
+
+
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        matrix = [[0 for _ in range(len(s))] for _ in range(numRows)]
-        row = 0
-        column = 0
-        if len(s) <= 2 or numRows==1:
+        if numRows == 1 or numRows >= len(s):
             return s
-        
-        forward = True
-        for i, char in enumerate(s):
-            # print("Row: ", row, "Column: ", column)
-            if forward:
-                if numRows - row != 1:
-                    matrix[row][column] = char
-                    row += 1
-                else:
-                    matrix[row][column] = char
-                    forward = False
 
-            if not forward:
-                if row != 0:
-                    matrix[row][column] = char
-                    row -= 1
-                    column += 1
-                else:
-                    matrix[row][column] = char
-                    row += 1
-                    forward = True
-            
-        new_str = ""
-        for row in matrix:
-            for value in row:
-                if value != 0:
-                    new_str += value
-                    # print(value, end = " ")
-            # print()
+        rows = ['' for _ in range(numRows)]
+        current_row = 0
+        going_down = False
 
-        return new_str
-        
+        # print(rows)
 
-print(Solution().convert("ABC", 1))
+        for char in s:
+            print(rows)
+
+            rows[current_row] += char
+            # Change direction if we're at the first or last row
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            current_row += 1 if going_down else -1
+
+
+        return ''.join(rows)
+
+
+
+
+print(Solution().convert("PAYPALISHIRING", 5))
 
 
 # Example 1:
